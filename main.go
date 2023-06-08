947992ddf3b8c2df341c171c7c9dea275748d171
fikrianggara <fikriseptriananggara@gmail.com>
2023-06-08 23:57:51 +0700

    add hello world api

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "hello from  '/' endpoint"
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "hello world"
+	w.Write([]byte(message))
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var message = "hello from '/' endpoint"
+		w.Write([]byte(message))
+	})
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var address = "localhost:9000"
+	fmt.Printf("server started at %s", address)
+	var err = http.ListenAndServe(address, nil)
+
+	if err != nil {
+		fmt.Println("error occured", err.Error())
+	}
 }
