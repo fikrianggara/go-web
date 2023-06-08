ddf4d5f85ddb3f093e5edc570967bb04b00e0d81
fikrianggara <fikriseptriananggara@gmail.com>
2023-06-09 00:50:40 +0700

    add basic rest api

--- main.go
+++ main.go
@@ -1,33 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	f "fmt"
 	"net/http"
 )
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	var message = "hello from  '/' endpoint"
-	w.Write([]byte(message))
-}
 
-func handlerHello(w http.ResponseWriter, r *http.Request) {
-	var message = "hello world"
-	w.Write([]byte(message))
+	var fikri Developer = Person{"Fikri", 22, []string{"coding", "sports", "music"}}
+	f.Println(fikri.Code() + "->" + fikri.Build() + "->" + fikri.Test() + "->" + fikri.Deploy())
+	jsonInBytes, err := json.Marshal(fikri.(Person))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonInBytes)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var message = "hello from '/' endpoint"
+	// routing
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		var message = "hello from '/index' endpoint"
 		w.Write([]byte(message))
 	})
-	http.HandleFunc("/index", handlerIndex)
-	http.HandleFunc("/hello", handlerHello)
 
-	var address = "localhost:9000"
-	fmt.Printf("server started at %s", address)
-	var err = http.ListenAndServe(address, nil)
+	var address = ":9000"
+	server := new(http.Server)
+	server.Addr = address
+	f.Printf("server started at %s", address)
+	var err = server.ListenAndServe()
 
 	if err != nil {
-		fmt.Println("error occured", err.Error())
+		f.Println("error occured", err.Error())
 	}
 }
---
+++ person.go
@@ -0,0 +1,27 @@
+package main
+
+type Person struct {
+	Name  string `json:"Name"`
+	Age   int
+	Hobby []string
+}
+
+type Developer interface {
+	Code() string
+	Build() string
+	Test() string
+	Deploy() string
+}
+
+func (p Person) Code() string {
+	return "coding"
+}
+func (p Person) Build() string {
+	return "building..."
+}
+func (p Person) Test() string {
+	return "testing..."
+}
+func (p Person) Deploy() string {
+	return "deploying..."
+}
